d09544420c10786758b0d474e75e40729e86dbea
William Lindner <wi-lindner@wiu.edu>
2018-10-08 19:04:30 -0400

    test commit

--- src/Video.php
+++ src/Video.php
@@ -30,7 +30,7 @@ SOFTWARE.
 
 -----------------------------------------------------
 
-EXAMPLE: 
+EXAMPLE:
 
 $video = new Video();
 $video->fetch('filename.mp4');
@@ -46,36 +46,37 @@ class Video
   public $state = null;
   public $error = null;
 
-  private $video;
+  private $__video;
 
-  public $fileSize = null;
+  public $fileSize     = null;
   public $streamBlocks = null;
   public $lastModified = null;
 
-  final function __construct($fileName = null)
+  final public function __construct($fileName = null)
   {
-    if($fileName) {
+    if ($fileName) {
       $this->fetch($fileName);
     }
   }
 
-  public function fetch($fileName = null) {
+  public function fetch($fileName = null)
+  {
     // check the filename integrity
     if (!$fileName || !is_string($fileName)) {
       throw new Exception('No file name provided.');
     }
     // set the path to the video and open it in read only state (binary)
-    $filePath = $_SERVER['DOCUMENT_ROOT'] . self::VIDEO_PATH . $fileName;
-    $this->video = fopen($filePath, 'rb') or die ('Could not open video file for reading.');
-    
+    $filePath    = $_SERVER['DOCUMENT_ROOT'] . self::VIDEO_PATH . $fileName;
+    $this->video = fopen($filePath, 'rb') or die('Could not open video file for reading.');
+
     // setup information about the file and return the instance
-    $fileSize = $this->fileSize = filesize($filePath);
+    $fileSize           = $this->fileSize           = filesize($filePath);
     $this->lastModified = filemtime($filePath);
     $this->streamBlocks = ceil($fileSize / self::BUFFER * 2 + 4);
   }
   /*
-  * Start streaming video content
-  */
+   * Start streaming video content
+   */
   public function play($type = 'video/mp4')
   {
     // flush everything if possible and setup the basic headers
@@ -86,8 +87,8 @@ class Video
     header("Expires: 0");
     header("Last-Modified: " . gmdate('D, d M Y H:i:s', $this->lastModified) . ' GMT');
 
-    $streamStartPoint  = 0;
-    $streamEndPoint    = $this->fileSize - 1;
+    $streamStartPoint = 0;
+    $streamEndPoint   = $this->fileSize - 1;
 
     // set the range in the header
     header("Accept-Ranges: 0-" . $streamEndPoint);
@@ -95,40 +96,40 @@ class Video
     if (isset($_SERVER['HTTP_RANGE'])) {
       $currentStreamStart = $streamStartPoint;
       $currentStreamEnd   = $streamEndPoint;
-  
+
       list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
       if (strpos($range, ',') !== false) {
         header('HTTP/1.1 416 Requested Range Not Satisfiable');
-        header("Content-Range: bytes " . $streamStartPoint . "-" . $streamEndPoint. "/" . $this->fileSize);
+        header("Content-Range: bytes " . $streamStartPoint . "-" . $streamEndPoint . "/" . $this->fileSize);
         exit;
       }
       if ($range == '-') {
         $currentStreamStart = $this->fileSize - substr($range, 1);
       } else {
-        $range   = explode('-', $range);
+        $range              = explode('-', $range);
         $currentStreamStart = $range[0];
-        $currentStreamEnd = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $currentStreamEnd;
+        $currentStreamEnd   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $currentStreamEnd;
       }
 
-      $currentStreamEnd = ($currentStreamEnd > $streamEndPoint) ? $streamEndPoint: $currentStreamEnd;
+      $currentStreamEnd = ($currentStreamEnd > $streamEndPoint) ? $streamEndPoint : $currentStreamEnd;
 
       if ($currentStreamStart > $currentStreamEnd || $currentStreamStart > $this->fileSize - 1 || $currentStreamEnd >= $this->fileSize) {
         header('HTTP/1.1 416 Requested Range Not Satisfiable');
-        header("Content-Range: bytes " . $streamStartPoint . "-" . $streamEndPoint. "/" . $this->fileSize);
+        header("Content-Range: bytes " . $streamStartPoint . "-" . $streamEndPoint . "/" . $this->fileSize);
         exit;
       }
 
       $streamStartPoint = $currentStreamStart;
-      $streamEndPoint  = $currentStreamEnd;
-      $length      = $streamEndPoint- $streamStartPoint + 1;
+      $streamEndPoint   = $currentStreamEnd;
+      $length           = $streamEndPoint - $streamStartPoint + 1;
       fseek($this->video, $streamStartPoint);
       header('HTTP/1.1 206 Partial Content');
       header("Content-Length: " . $length);
-      header("Content-Range: bytes " . $streamStartPoint . "-" . $streamEndPoint. "/" . $this->fileSize);
-      } else {
-        header("Content-Length: " . $this->fileSize);
-      }
-  
+      header("Content-Range: bytes " . $streamStartPoint . "-" . $streamEndPoint . "/" . $this->fileSize);
+    } else {
+      header("Content-Length: " . $this->fileSize);
+    }
+
     $endPointer = $streamStartPoint;
     set_time_limit(0);
 
@@ -144,5 +145,5 @@ class Video
     }
 
     fclose($this->video);
- }
+  }
 }
